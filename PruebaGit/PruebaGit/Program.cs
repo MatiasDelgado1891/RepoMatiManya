b0a6cebe1f5e60204da547ab929ad477ab72e9ce
aguante el manya <matiasdelgadocastillo1891@gmail.com>
2023-03-30 19:48:10 -0300

    primer commit

---
+++ PruebaGit/PruebaGit/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("aguante el manya");
+        
+        }
+    
+    
+    
+    
+    }
+}
